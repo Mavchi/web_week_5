c6d498edaf8a021a673ee16c3fd28dc82113819a
Aleksander Khlebnikov <aleksi.khlebnikov@gmail.com>
2022-10-02 19:07:16 +0000

    Update 35 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
 import "./styles.css";
+import "./leaflet/leaflet.css"
+import "./leaflet/leaflet"
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const fetchData = async () => {
+    const url = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326"
+
+    const response = await fetch(url);
+    const geoData = await response.json();
+    
+    initMap(geoData)
+}
+
+const initMap = (data) => {
+    let map = L.map('map', {
+        minZoom: -3
+    })
+
+    let geoJson = L.geoJSON(data, {
+        style: getStyle
+    }).addTo(map)
+
+    let osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: "© OpenStreetMap"
+    }).addTo(map);
+
+    let google = L.tileLayer("https://{s}.google.com/vt/lyrs=s@221097413,traffic&x={x}&y={y}&z={z}", {
+        maxZoom: 20,
+        minZoom: 2,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    }).addTo(map)
+
+    let baseMaps = {
+        "OpenStreetMap": osm,
+        "Google Maps": google
+    }
+
+    let overlayMaps = {
+        "LUT": geoJson
+    }
+
+    let layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+
+    map.fitBounds(geoJson.getBounds())
+
+}
+
+const getStyle = (feature) => {
+    return {
+        weight: 2
+    }
+}
+
+fetchData()
